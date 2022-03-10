b94510362cf1d6eeca17b9225eda56732a694a7c
Saul Sandoval <ssandovalsalazar@gmail.com>
2022-03-10 17:14:58 -0600

    new file test added

---
+++ site.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $name = "Roberto";
+            $age  = 32;
+            echo "Hello $name !!! <br>";
+            echo "You are $age";      
+         ?>
+    </body>
+</html>
